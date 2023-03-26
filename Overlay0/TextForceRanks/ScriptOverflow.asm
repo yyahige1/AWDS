8223b1629e3feea9d30210c74d6a1ab74e5d69a2
yyahige1 <66831152+yyahige1@users.noreply.github.com>
2023-03-26 14:19:17 +0200

    Add files via upload

---
+++ Overlay0/TextForceRanks/ScriptOverflow.asm
@@ -0,0 +1,26 @@
+;ScriptOverflow.asm
+ScriptForceDescription180:
+	.ascii	"Cause snow in S/COP",0x0
+ScriptForceDescription181:
+	.ascii	"Cause sand in S/COP",0x0
+ScriptForceDescription182:
+	.ascii	"Less move in S/COP",0x0
+ScriptForceDescription183:
+	.ascii	"+1 enemy terrain stars",0x0
+ScriptForceDescription184:
+	.ascii	"More range in S/COP",0x0
+ScriptForceDescription185:
+	.ascii	"+5% Terrain star attack",0x0
+ScriptForceDescription186:
+	.ascii	"+25% Counter-atk damage",0x0
+ScriptForceDescription187:
+	.ascii	"-25% Counter-atk damage",0x0
+ScriptForceDescription188:
+	.ascii	"Build copters in S/COP",0x0
+ScriptForceDescription189:
+	.ascii	"Random handicap skill",0x0
+ScriptForceDescription190:
+	.ascii	"Random powerup skill",0x0
+ScriptForceDescription191:
+	.ascii	"2x Equipped skill power",0x0
+	
---
+++ Overlay0/TextForceRanks/script.asm
@@ -0,0 +1,566 @@
+;scriptname.asm
+;Force Rank Script Name Data
+.org 0x02308B08
+ScriptForceName000:
+ScriptForceName001:
+ScriptForceName002:
+ScriptForceName003:
+ScriptForceName004:
+ScriptForceName005:
+ScriptForceName006:
+ScriptForceName007:
+ScriptForceName008:
+ScriptForceName009:
+ScriptForceName010:
+ScriptForceName011:
+ScriptForceName012:
+ScriptForceName013:
+ScriptForceName014:
+ScriptForceName015:
+ScriptForceName016:
+	.ascii	"Blank",0x0
+ScriptForceName017:
+	.ascii	"Bruiser",0x0
+ScriptForceName018:
+	.ascii	"Sharpshooter",0x0
+ScriptForceName019:
+	.ascii	"Luck",0x0
+ScriptForceName020:
+	.ascii	"Slam Guard",0x0
+ScriptForceName021:
+	.ascii	"Snipe Guard",0x0
+ScriptForceName022:
+	.ascii	"Combat Pay",0x0
+ScriptForceName023:
+	.ascii	"Sneaky",0x0
+ScriptForceName024:
+	.ascii	"Tower Power",0x0
+ScriptForceName025:
+	.ascii	"APC Guard",0x0
+ScriptForceName026:
+	.ascii	"Teamwork",0x0
+ScriptForceName027:
+	.ascii	"Sale Price",0x0
+ScriptForceName028:
+	.ascii	"Mechanic",0x0
+ScriptForceName029:
+	.ascii	"Scout",0x0
+ScriptForceName030:
+	.ascii	"Invader",0x0
+ScriptForceName031:
+	.ascii	"Seamanship",0x0
+ScriptForceName032:
+	.ascii	"Road Rage",0x0
+ScriptForceName033:
+	.ascii	"Urban Fighter",0x0
+ScriptForceName034:
+	.ascii	"Mountaineer",0x0
+ScriptForceName035:
+	.ascii	"Ranger",0x0
+ScriptForceName036:
+	.ascii	"APC Boost",0x0
+ScriptForceName037:
+	.ascii	"Star Power",0x0
+ScriptForceName038:
+	.ascii	"Missile Guard",0x0
+ScriptForceName039:
+	.ascii	"Cannon Guard",0x0
+ScriptForceName040:
+	.ascii	"Stealthy",0x0
+ScriptForceName041:
+	.ascii	"Pathfinder",0x0
+ScriptForceName042:
+	.ascii	"Prairie Dog",0x0
+ScriptForceName043:
+	.ascii	"Bodyguard",0x0
+ScriptForceName044:
+	.ascii	"Gold Rush",0x0
+ScriptForceName045:
+	.ascii	"Icebreaker",0x0
+ScriptForceName046:
+	.ascii	"High and Dry",0x0
+ScriptForceName047:
+	.ascii	"Sand Scorpion",0x0
+ScriptForceName048:
+	.ascii	"Slam Shield",0x0
+ScriptForceName049:
+	.ascii	"Snipe Shield",0x0
+ScriptForceName050:
+	.ascii	"Brawler",0x0
+ScriptForceName051:
+	.ascii	"Sniper",0x0
+ScriptForceName052:
+	.ascii	"Synergy",0x0
+ScriptForceName053:
+	.ascii	"Fire Sale",0x0
+ScriptForceName054:
+	.ascii	"Gear Head",0x0
+ScriptForceName055:
+	.ascii	"Eagle Eye",0x0
+ScriptForceName056:
+	.ascii	"Conqueror",0x0
+ScriptForceName057:
+ScriptForceName058:
+ScriptForceName059:
+ScriptForceName060:
+	.ascii	"Blank",0x0
+ScriptForceName061:
+	.ascii	"Backstab",0x0
+ScriptForceName062:
+	.ascii	"Mistwalker",0x0
+ScriptForceName063:
+	.ascii	"Soul of Hachi",0x0
+ScriptForceName064:
+ScriptForceName065:
+ScriptForceName066:
+ScriptForceName067:
+ScriptForceName068:
+ScriptForceName069:
+ScriptForceName070:
+ScriptForceName071:
+ScriptForceName072:
+ScriptForceName073:
+ScriptForceName074:
+ScriptForceName075:
+ScriptForceName076:
+ScriptForceName077:
+ScriptForceName078:
+ScriptForceName079:
+ScriptForceName080:
+ScriptForceName081:
+ScriptForceName082:
+ScriptForceName083:
+ScriptForceName084:
+ScriptForceName085:
+ScriptForceName086:
+ScriptForceName087:
+ScriptForceName088:
+ScriptForceName089:
+ScriptForceName090:
+ScriptForceName091:
+ScriptForceName092:
+ScriptForceName093:
+ScriptForceName094:
+ScriptForceName095:
+ScriptForceName096:
+ScriptForceName097:
+ScriptForceName098:
+ScriptForceName099:
+ScriptForceName100:
+ScriptForceName101:
+ScriptForceName102:
+ScriptForceName103:
+ScriptForceName104:
+ScriptForceName105:
+ScriptForceName106:
+ScriptForceName107:
+ScriptForceName108:
+ScriptForceName109:
+ScriptForceName110:
+ScriptForceName111:
+ScriptForceName112:
+ScriptForceName113:
+ScriptForceName114:
+ScriptForceName115:
+ScriptForceName116:
+ScriptForceName117:
+ScriptForceName118:
+ScriptForceName119:
+ScriptForceName120:
+ScriptForceName121:
+ScriptForceName122:
+ScriptForceName123:
+ScriptForceName124:
+ScriptForceName125:
+ScriptForceName126:
+ScriptForceName127:
+ScriptForceName128:
+ScriptForceName129:
+ScriptForceName130:
+ScriptForceName131:
+ScriptForceName132:
+ScriptForceName133:
+ScriptForceName134:
+ScriptForceName135:
+ScriptForceName136:
+ScriptForceName137:
+ScriptForceName138:
+	.ascii	"Name",0x0
+ScriptForceName139:
+	.ascii	"Income Tax",0x0
+ScriptForceName140:
+	.ascii	"Tax Hike",0x0
+ScriptForceName141:
+	.ascii	"Null Luck",0x0
+ScriptForceName142:
+	.ascii	"Star Drain",0x0
+ScriptForceName143:
+	.ascii	"Frostbitten",0x0
+ScriptForceName144:
+	.ascii	"Sopping Wet",0x0
+ScriptForceName145:
+	.ascii	"Sandy Eyes",0x0
+ScriptForceName146:
+	.ascii	"Softie",0x0
+ScriptForceName147:
+	.ascii	"Glass Eyes",0x0
+ScriptForceName148:
+	.ascii	"Reparations",0x0
+ScriptForceName149:
+	.ascii	"Pure Power",0x0
+ScriptForceName150:
+	.ascii	"True Grit",0x0
+ScriptForceName151:
+	.ascii	"Underpowered",0x0
+ScriptForceName152:
+	.ascii	"Flimsy",0x0
+ScriptForceName153:
+	.ascii	"Glass Jaw",0x0
+ScriptForceName154:
+	.ascii	"Unusual Hat",0x0
+ScriptForceName155:
+	.ascii	"Light Mist",0x0
+ScriptForceName156:
+	.ascii	"Duct Tape",0x0
+ScriptForceName157:
+	.ascii	"Hippy",0x0
+ScriptForceName158:
+	.ascii	"Pacifist",0x0
+ScriptForceName159:
+	.ascii	"Drunk Driver",0x0
+ScriptForceName160:
+	.ascii	"Sickly",0x0
+ScriptForceName161:
+	.ascii	"Country Hick",0x0
+ScriptForceName162:
+	.ascii	"No Sherpa",0x0
+ScriptForceName163:
+	.ascii	"Seasick",0x0
+ScriptForceName164:
+	.ascii	"Flat Tires",0x0
+ScriptForceName165:
+	.ascii	"Signal Loss",0x0
+ScriptForceName166:
+	.ascii	"Facestab",0x0
+ScriptForceName167:
+	.ascii	"Banshee",0x0
+ScriptForceName168:
+	.ascii	"Classic APC",0x0
+ScriptForceName169:
+	.ascii	"Commando",0x0
+ScriptForceName170:
+	.ascii	"Tank Ops",0x0
+ScriptForceName171:
+	.ascii	"Smokescreen",0x0
+ScriptForceName172:
+	.ascii	"Ace Pilots",0x0
+ScriptForceName173:
+	.ascii	"Admiral",0x0
+ScriptForceName174:
+	.ascii	"Cursed",0x0
+ScriptForceName175:
+	.ascii	"Cover Identity",0x0
+ScriptForceName176:
+	.ascii	"Enflakenator",0x0
+ScriptForceName177:
+	.ascii	"Speed Demon",0x0
+ScriptForceName178:
+	.ascii	"Espionage",0x0
+ScriptForceName179:
+	.ascii	"Rain Dance",0x0
+ScriptForceName180:
+	.ascii	"Flurry",0x0
+ScriptForceName181:
+	.ascii	"Desert Lord",0x0
+ScriptForceName182:
+	.ascii	"Stumbler",0x0
+ScriptForceName183:
+	.ascii	"Intel Leak",0x0
+ScriptForceName184:
+	.ascii	"Trick Shot",0x0
+ScriptForceName185:
+	.ascii	"Terrain Star",0x0
+ScriptForceName186:
+	.ascii	"Revenge",0x0
+ScriptForceName187:
+	.ascii	"Surrender",0x0
+ScriptForceName188:
+	.ascii	"Helipad",0x0
+ScriptForceName189:
+	.ascii	"Random Nerf",0x0
+ScriptForceName190:
+	.ascii	"Random Boost",0x0
+ScriptForceName191:
+	.ascii	"Empowerer",0x0
+
+;Force Rank Description Data
+.org	0x02309424
+ScriptForceDescription000:
+ScriptForceDescription001:
+ScriptForceDescription002:
+ScriptForceDescription003:
+ScriptForceDescription004:
+ScriptForceDescription005:
+ScriptForceDescription006:
+ScriptForceDescription007:
+ScriptForceDescription008:
+ScriptForceDescription009:
+ScriptForceDescription010:
+ScriptForceDescription011:
+ScriptForceDescription012:
+ScriptForceDescription013:
+ScriptForceDescription014:
+ScriptForceDescription015:
+ScriptForceDescription016:
+	.ascii	"Blank",0x0
+ScriptForceDescription017:
+	.ascii	"Direct attack +5%",0x0;"Bruiser",0x0
+ScriptForceDescription018:
+	.ascii	"Indirect attack +5%",0x0;"Sharpshooter",0x0
+ScriptForceDescription019:
+	.ascii	"10% Random higher damage",0x0;"Luck",0x0
+ScriptForceDescription020:
+	.ascii	"Direct-fire defence +5%",0x0;"Slam Guard",0x0
+ScriptForceDescription021:
+	.ascii	"Indirect-fire defence +5%",0x0;"Snipe Guard",0x0
+ScriptForceDescription022:
+	.ascii	"Damage foes, earn 2% cash",0x0;"Combat Pay",0x0
+ScriptForceDescription023:
+	.ascii	"Dive/hide fuel cost -1",0x0;"Sneaky",0x0
+ScriptForceDescription024:
+	.ascii	"Com Tower effect +5%",0x0;"Tower Power",0x0
+ScriptForceDescription025:
+	.ascii	"Transport defence +20%",0x0;"APC Guard",0x0
+ScriptForceDescription026:
+	.ascii	"Tag CO's attack +5%",0x0;"Teamwork",0x0
+ScriptForceDescription027:
+	.ascii	"Production cost -5%",0x0;"Sale Price",0x0
+ScriptForceDescription028:
+	.ascii	"Base repair +1",0x0;"Mechanic",0x0
+ScriptForceDescription029:
+	.ascii	"Vision +1",0x0;"Scout",0x0
+ScriptForceDescription030:
+	.ascii	"Capture +1",0x0;"Invader",0x0
+ScriptForceDescription031:
+	.ascii	"Sea attack +10%",0x0;"Seamanship",0x0
+ScriptForceDescription032:
+	.ascii	"Road attack +10%",0x0;"Road Rage",0x0
+ScriptForceDescription033:
+	.ascii	"City attack +10%",0x0;"Urban Fighter",0x0
+ScriptForceDescription034:
+	.ascii	"Mountain attack +10%",0x0;"Mountaineer",0x0
+ScriptForceDescription035:
+	.ascii	"Woods attack +10%",0x0;"Ranger",0x0
+ScriptForceDescription036:
+	.ascii	"Transport movement +1",0x0;"APC Boost",0x0
+ScriptForceDescription037:
+	.ascii	"+20% Power meter fill",0x0;"Star Power",0x0
+ScriptForceDescription038:
+	.ascii	"Damage from silos -1",0x0;"Missile Guard",0x0
+ScriptForceDescription039:
+	.ascii	"Damage from cannons -2",0x0;"Cannon Guard",0x0
+ScriptForceDescription040:
+	.ascii	"Dive/hide cost -2",0x0;"Stealthy",0x0
+ScriptForceDescription041:
+	.ascii	"Plain movement = 1",0x0;"Pathfinder",0x0
+ScriptForceDescription042:
+	.ascii	"Woods movement = 1",0x0;"Prairie Dog",0x0
+ScriptForceDescription043:
+	.ascii	"Tag CO's defence +10%",0x0;"Bodyguard",0x0
+ScriptForceDescription044:
+	.ascii	"Funds from bases +100",0x0;"Gold Rush",0x0
+ScriptForceDescription045:
+	.ascii	"Attack in snow +20%",0x0;"Icebreaker",0x0
+ScriptForceDescription046:
+	.ascii	"Attack in rain +20%",0x0;"High and Dry",0x0
+ScriptForceDescription047:
+	.ascii	"Attack in sandstorm +20%",0x0;"Sand Scorpion",0x0
+ScriptForceDescription048:
+	.ascii	"Direct-fire defence +10%",0x0;"Slam Shield",0x0
+ScriptForceDescription049:
+	.ascii	"Indirect-fire defence +10%",0x0;"Snipe Shield",0x0
+ScriptForceDescription050:
+	.ascii	"Direct attack +10%",0x0;"Brawler",0x0
+ScriptForceDescription051:
+	.ascii	"Indirect attack +10%",0x0;"Sniper",0x0
+ScriptForceDescription052:
+	.ascii	"Tag CO's attack +10%",0x0;"Synergy",0x0
+ScriptForceDescription053:
+	.ascii	"Production cost -10%",0x0;"Fire Sale",0x0
+ScriptForceDescription054:
+	.ascii	"Base repair +2",0x0;"Gear Head",0x0
+ScriptForceDescription055:
+	.ascii	"Vision +2",0x0;"Eagle Eye",0x0
+ScriptForceDescription056:
+	.ascii	"Capture +2",0x0;"Conqueror",0x0
+ScriptForceDescription057:
+ScriptForceDescription058:
+ScriptForceDescription059:
+ScriptForceDescription060:
+	.ascii	"Desc",0x0
+ScriptForceDescription061:
+	.ascii	"Dive/hide attack +20%",0x0;"Backstab",0x0
+ScriptForceDescription062:
+	.ascii	"Counter first during SCOP",0x0;"Mistwalker",0x0
+ScriptForceDescription063:
+	.ascii	"Deploy from cities in SCOP",0x0;"Soul of Hachi",0x0
+ScriptForceDescription064:
+ScriptForceDescription065:
+ScriptForceDescription066:
+ScriptForceDescription067:
+ScriptForceDescription068:
+ScriptForceDescription069:
+ScriptForceDescription070:
+ScriptForceDescription071:
+ScriptForceDescription072:
+ScriptForceDescription073:
+ScriptForceDescription074:
+ScriptForceDescription075:
+ScriptForceDescription076:
+ScriptForceDescription077:
+ScriptForceDescription078:
+ScriptForceDescription079:
+ScriptForceDescription080:
+ScriptForceDescription081:
+ScriptForceDescription082:
+ScriptForceDescription083:
+ScriptForceDescription084:
+ScriptForceDescription085:
+ScriptForceDescription086:
+ScriptForceDescription087:
+ScriptForceDescription088:
+ScriptForceDescription089:
+ScriptForceDescription090:
+ScriptForceDescription091:
+ScriptForceDescription092:
+ScriptForceDescription093:
+ScriptForceDescription094:
+ScriptForceDescription095:
+ScriptForceDescription096:
+ScriptForceDescription097:
+ScriptForceDescription098:
+ScriptForceDescription099:
+ScriptForceDescription100:
+ScriptForceDescription101:
+ScriptForceDescription102:
+ScriptForceDescription103:
+ScriptForceDescription104:
+ScriptForceDescription105:
+ScriptForceDescription106:
+ScriptForceDescription107:
+ScriptForceDescription108:
+ScriptForceDescription109:
+ScriptForceDescription110:
+ScriptForceDescription111:
+ScriptForceDescription112:
+ScriptForceDescription113:
+ScriptForceDescription114:
+ScriptForceDescription115:
+ScriptForceDescription116:
+ScriptForceDescription117:
+ScriptForceDescription118:
+ScriptForceDescription119:
+ScriptForceDescription120:
+ScriptForceDescription121:
+ScriptForceDescription122:
+ScriptForceDescription123:
+ScriptForceDescription124:
+ScriptForceDescription125:
+ScriptForceDescription126:
+ScriptForceDescription127:
+ScriptForceDescription128:
+ScriptForceDescription129:
+ScriptForceDescription130:
+ScriptForceDescription131:
+ScriptForceDescription132:
+ScriptForceDescription133:
+ScriptForceDescription134:
+ScriptForceDescription135:
+ScriptForceDescription136:
+ScriptForceDescription137:
+ScriptForceDescription138:
+	.ascii	"Desc",0x0
+ScriptForceDescription139:
+	.ascii	"Funds from bases -100",0x0
+ScriptForceDescription140:
+	.ascii	"Production cost +10%",0x0
+ScriptForceDescription141:
+	.ascii	"10% Less bonus damage",0x0
+ScriptForceDescription142:
+	.ascii	"-20% Power meter fill",0x0
+ScriptForceDescription143:
+	.ascii	"Attack in snow -20%",0x0
+ScriptForceDescription144:
+	.ascii	"Attack in rain -20%",0x0
+ScriptForceDescription145:
+	.ascii	"Attack in sandstorm -20%",0x0
+ScriptForceDescription146:
+	.ascii	"Direct attack -10%",0x0
+ScriptForceDescription147:
+	.ascii	"Indirect attack -10%",0x0
+ScriptForceDescription148:
+	.ascii	"Damage foes to lose cash",0x0
+ScriptForceDescription149:
+	.ascii	"Attack +10%, slow CO Powers",0x0
+ScriptForceDescription150:
+	.ascii	"Defence +10%, slow CO Powers",0x0
+ScriptForceDescription151:
+	.ascii	"Attack -10%, fast CO Powers",0x0
+ScriptForceDescription152:
+	.ascii	"Defence -10%, fast CO Powers",0x0
+ScriptForceDescription153:
+	.ascii	"Direct-fire defence -10%",0x0
+ScriptForceDescription154:
+	.ascii	"Indirect-fire defence -10%",0x0
+ScriptForceDescription155:
+	.ascii	"Vision -1",0x0
+ScriptForceDescription156:
+	.ascii	"Repair -1",0x0
+ScriptForceDescription157:
+	.ascii	"Capture -1",0x0
+ScriptForceDescription158:
+	.ascii	"Capture -2",0x0
+ScriptForceDescription159:
+	.ascii	"Road attack -10%",0x0
+ScriptForceDescription160:
+	.ascii	"Woods attack -10%",0x0
+ScriptForceDescription161:
+	.ascii	"City attack -10%",0x0
+ScriptForceDescription162:
+	.ascii	"Mountain attack -10%",0x0
+ScriptForceDescription163:
+	.ascii	"Sea attack -10%",0x0
+ScriptForceDescription164:
+	.ascii	"Transport movement -1",0x0
+ScriptForceDescription165:
+	.ascii	"Com tower effect -5%",0x0
+ScriptForceDescription166:
+	.ascii	"Dive/hide attack -20%",0x0
+ScriptForceDescription167:
+	.ascii	"Dive/hide fuel cost +2",0x0
+ScriptForceDescription168:
+	.ascii	"Transport defence -20%",0x0
+ScriptForceDescription169:
+	.ascii	"Foot soldier damage +5",0x0
+ScriptForceDescription170:
+	.ascii	"Tank class damage +5",0x0
+ScriptForceDescription171:
+	.ascii	"Artillery class damage +5",0x0
+ScriptForceDescription172:
+	.ascii	"Aircraft damage +5",0x0
+ScriptForceDescription173:
+	.ascii	"Ship damage +5",0x0
+ScriptForceDescription174:
+	.ascii	"10% Min. random damage",0x0
+ScriptForceDescription175:
+	.ascii	"Hide HP intel from foes",0x0
+ScriptForceDescription176:
+	.ascii	"Random dmg, more in S/COP",0x0
+ScriptForceDescription177:
+	.ascii	"Extra movement in S/COP",0x0
+ScriptForceDescription178:
+	.ascii	"-1 Enemy terrain stars",0x0
+ScriptForceDescription179:
+	.ascii	"Cause rain in S/COP",0x0
+
+;blank line
